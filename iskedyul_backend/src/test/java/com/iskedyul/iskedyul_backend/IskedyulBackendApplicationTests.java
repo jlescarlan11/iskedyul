78580b78b883b11a47011b93504b03ab448730f0
john lester escarlan <jlescarlan11@gmail.com>
2025-08-31 22:27:03 +0800

    backend_init

---
+++ iskedyul_backend/src/test/java/com/iskedyul/iskedyul_backend/IskedyulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iskedyul.iskedyul_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IskedyulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
